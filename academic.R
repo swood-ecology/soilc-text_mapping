0ece53480ef302b3ecb1b4e9997d071e26845b0c
Stephen Wood <stephen.wood@CT201-SWOOD.local>
2017-11-10 10:28:21 -0500

    Minor changes

--- academic.R
+++ academic.R
@@ -1,6 +1,8 @@
 # library("devtools")
 # install_github("massimoaria/bibliometrix")
 library(bibliometrix) # For reading and analyzing ISI stuff
+library(metagear)     # For downloading PDFs
+library(RefManageR)   # For writing .bib files
 
 
 ## *********** ##
@@ -27,30 +29,42 @@ soil.health <- soil.health[order(soil.health$PY),]
 # Create a co-citation network
 ref.co.cite.network <- biblioNetwork(soil.health, analysis = "co-citation", network = "references", sep=".  ")
 # # Normalize network for papers with different numbers of references
-S <- normalizeSimilarity(ref.co.cite.network)
+# S <- normalizeSimilarity(ref.co.cite.network)
 # Plot network
-networkPlot(ref.co.cite.network, n = 20, Title = "Co-Citation Network", type = "fruchterman", size=T, remove.multiple=T)
+networkPlot(ref.co.cite.network, n = 10, Title = "Co-Citation Network", type = "fruchterman", size=T, remove.multiple=T)
 
 ## Co-occurrences among keywords
 sh.keywords <- biblioNetwork(soil.health, analysis = "co-occurrences", network = "keywords", sep = ";")
-networkPlot(sh.keywords, n = 20, Title = "Keyword Co-occurrences", type = "kamada", size=T)
+networkPlot(sh.keywords, n = 15, Title = "Keyword Co-occurrences", type = "kamada", size=T)
 
 ## Abstract term co-occurences
 sh.abstract <- biblioNetwork(termExtraction(soil.health,Field="AB"), analysis = "co-occurrences", network = "abstracts")
-networkPlot(sh.abstract, n = 20, Title = "Abstract Co-occurrences", type = "kamada", size=T)
-
-## Conceptual map
-CS <- conceptualStructure(termExtraction(soil.health,Field="AB"))
+sh.abstract.2 <- sh.abstract[which((rownames(sh.abstract)!='STUDY') & (rownames(sh.abstract)!='SOILS') & (rownames(sh.abstract)!='SOIL') & (rownames(sh.abstract)!='HEALTH') & (rownames(sh.abstract)!='ELSEVIER')),which((colnames(sh.abstract)!='STUDY') & (colnames(sh.abstract)!='SOILS') &(colnames(sh.abstract)!='SOIL') & (colnames(sh.abstract)!='HEALTH') & (rownames(sh.abstract)!='ELSEVIER'))]
+networkPlot(sh.abstract.2, n = 15, Title = "Abstract Co-occurrences", type = "kamada", size=T)
 
 ## Divide up into time analysis
-time.data <- timeslice(soil.health,breaks=c(1990,2000,2010))
+time.data <- timeslice(soil.health,breaks=c(2000,2010))
+plot(biblioAnalysis(time.data[[1]], sep = ";"),pause=F)
+preoughts <- networkPlot(biblioNetwork(time.data[[1]], analysis = "co-occurrences", network = "keywords", sep = ";"),n=15,Title = "Pre-2000s Keyword Co-occurrences", type = "kamada", size=T)
+oughts <- networkPlot(biblioNetwork(time.data[[2]], analysis = "co-occurrences", network = "keywords", sep = ";"),n=15,Title = "2000s Keyword Co-occurrences", type = "kamada", size=T)
+tens <- networkPlot(biblioNetwork(time.data[[3]], analysis = "co-occurrences", network = "keywords", sep = ";"),n=15,Title = "2010s Keyword Co-occurrences", type = "kamada", size=T)
+
 
 ## Download papers
 data <- data.frame(paste(gsub(";.*$", "", soil.health$AU),soil.health$PY,soil.health$JI),soil.health$DI)
 names(data) <- c('Name','DOI')
-library(metagear)
+
 # nrow(data[which(data$DOI != '<NA>'),]) Of 1463 observations, 1038 have valid DOIs
 PDFs_collect(aDataFrame=data,DOIcolumn="DOI",FileNamecolumn="Name",directory="~/Google Drive/SNAPP-Soil-Carbon/Literature/soil_health/")
+
+# Re-read .bib files
+ReadBib("~/Google Drive/SNAPP-Soil-Carbon/Literature/soil_health/isi_searches/soil.health_healthy.soil_1to500.bib",check=FALSE)
+
+soil.health <- rbind(,
+                     isibib2df(readFiles("~/Google Drive/SNAPP-Soil-Carbon/Literature/soil_health/isi_searches/soil.health_healthy.soil_501to1000.bib")),
+                     isibib2df(readFiles("~/Google Drive/SNAPP-Soil-Carbon/Literature/soil_health/isi_searches/soil.health_healthy.soil_1001to1463.bib")))
+
+WriteBib()
 
 
 
